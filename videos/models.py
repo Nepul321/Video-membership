fbf18b38702b80b1c39f576dfd16f025c9308610
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-12-30 16:06:54 -0500

    Create videos db model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    youtube_url = models.URLField("Youtube URL")
+    description = models.TextField(blank=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+    available = models.BooleanField(default=True)
