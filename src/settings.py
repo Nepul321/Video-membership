901af45e92b43abc24e64ee04986af8535e39760
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-01 00:07:49 -0500

    Add bootstrap and some javascript

--- src/settings.py
+++ src/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+LOGIN_URL = 'accounts-login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 
 # Application definition
---
+++ static/js/auth/login.js
@@ -0,0 +1,10 @@
+const password = document.getElementById("id_password")
+const username = document.getElementById("id_username")
+
+
+function styleElements() {
+    password.classList.add("form-control")
+    username.classList.add("form-control")
+}
+
+styleElements();
