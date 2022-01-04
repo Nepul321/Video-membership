a98d3603c8eb385ada8ed5c00839472cefcfd1c8
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-03 20:44:49 -0500

    Setup local media storage

--- src/settings.py
+++ src/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 if DEBUG:
     STATICFILES_DIRS = [
@@ -142,6 +143,7 @@ if DEBUG:
     ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- static/js/videos/videos.js
+++ static/js/videos/videos.js
@@ -1,9 +1,26 @@
 const root = document.getElementById("root")
 
 function getVideos() {
-
+    const xhr = new XMLHttpRequest();
+    const method = "GET";
+    const url = '/api/videos/'
+    xhr.open(method, url)
+    xhr.onload = () => {
+       const serverResponse = xhr.response;
+       var listedItems = serverResponse;
+       if (xhr.status === 200) {
+           listedItems = JSON.parse(listedItems)
+           insertVideos(listedItems);
+       } else {
+           alert("An error occured")
+       }
+    }
+   
+    xhr.send();
 }
 
-function insertVideos() {
-    
+function insertVideos(data) {
+    console.log(data)
 }
+
+getVideos();
