1a41576e759dddd03dabef6ac95024f6a1af7b0c
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-05 17:30:19 -0500

    Finish playlists page

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -1,3 +1,4 @@
+from inspect import currentframe
 from django.db import reset_queries
 from rest_framework import serializers
 from .models import (
@@ -6,9 +7,10 @@ from .models import (
 
 class PlayListSerializer(serializers.ModelSerializer):
     videos = serializers.SerializerMethodField(read_only=True)
+    thumbnail = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = PlayList
-        fields = ('id', 'title', 'date', 'videos')
+        fields = ('id', 'title', 'date', 'videos', 'thumbnail')
 
     def get_videos(self, obj):
         videos = obj.videos.all()
@@ -17,9 +19,18 @@ class PlayListSerializer(serializers.ModelSerializer):
             item = {
                 "id" : video.id,
                 "title" : video.title,
-                "thumbnail_image" : video.thumbnail_image or None,
+                "thumbnail_image" : video.thumbnail_image.url or None,
                 "date" : video.date,
                 "views" : video.views
             }
             results.append(item)
         return results
+
+    def get_thumbnail(self, obj):
+        videos = obj.videos.all()
+        url = ""
+        if videos:
+            current_obj = videos.first()
+            url = current_obj.thumbnail_image.url
+
+        return url
--- static/js/playlists/playlists.js
+++ static/js/playlists/playlists.js
@@ -1,11 +1,55 @@
 const root = document.getElementById("root")
 
 function getPlaylists() {
-
+    const xhr = new XMLHttpRequest();
+    const method = "GET";
+    const url = '/api/playlists/'
+    xhr.open(method, url)
+    xhr.onload = () => {
+       const serverResponse = xhr.response;
+       var listedItems = serverResponse;
+       if (xhr.status === 200) {
+           listedItems = JSON.parse(listedItems)
+           insertPlaylists(listedItems);
+       } else {
+           alert("An error occured")
+       }
+    }
+   
+    xhr.send();
 }
 
-function insertPlaylists() {
+function insertPlaylists(data) {
+    data.forEach((item) => {
+            const card = document.createElement("div")
+            card.className = "card mb-3"
+            const row = document.createElement("div")
+            row.className = "row g-0"
+            const column = document.createElement("div")
+            column.className = "col-md-4"
+            const column_2 = document.createElement("div")
+            column_2.className = "col-md-8"
+            const card_body = document.createElement("div")
+            card_body.className = "card-body"
+            
+            column.innerHTML = `
+            <img src=${item.thumbnail} class="img-fluid rounded-start" alt="..." />
+            `
     
+            card_body.innerHTML = `
+            <p style="font-size: 25px;" class="card-title"><a href="">${item.title}</a></p>
+            <p class="small text-muted">${item.date}</p>
+            `
+            column_2.appendChild(card_body)
+    
+            row.appendChild(column)
+            row.appendChild(column_2)
+            card.appendChild(row)
+    
+            root.appendChild(card)
+    })
+
+    console.log(data)
 }
 
-console.log(root)
+getPlaylists();
