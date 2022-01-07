70381268bb2cc587359a416c8238eb7ef05846c9
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-07 12:39:33 -0500

    Create a playlist creation form

--- pages/forms.py
+++ pages/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from videos.models import Video
+from playlists.models import PlayList
 
 class VideoCreateForm(forms.ModelForm):
     class Meta:
@@ -11,4 +12,13 @@ class VideoCreateForm(forms.ModelForm):
             'youtube_url' : forms.URLInput(attrs={'class' : 'form-control'}),
             'description' : forms.Textarea(attrs={'class' : 'form-control'}),
             'available' : forms.CheckboxInput(attrs={'class' : 'form-check'})
+        }
+
+class PlayListCreateForm(forms.ModelForm):
+    class Meta:
+        model = PlayList
+        fields = ('title', )
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class' : 'form-control'}),
         }
