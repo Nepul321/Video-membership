93ae23bff1bb1a8331a41b5ca84e41c1daad3ece
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-01-11 19:21:26 -0500

    Update login.js

--- static/js/auth/login.js
+++ static/js/auth/login.js
@@ -1,9 +1,9 @@
-const password = document.getElementById("id_password");
-const username = document.getElementById("id_username");
+const password_input = document.getElementById("id_password");
+const username_input = document.getElementById("id_username");
 
 function styleElements() {
-  password.classList.add("form-control");
-  username.classList.add("form-control");
+  password_input.classList.add("form-control");
+  username_input.classList.add("form-control");
 }
 
 styleElements();
